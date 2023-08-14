bcf839ff32cf1fb0498356dcd038f4ab05bc627f
akshitpatel01 <akshitpatel01@gmail.com>
2023-08-14 11:03:21 +0530

    Revamp

---
+++ dev_install_docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+
